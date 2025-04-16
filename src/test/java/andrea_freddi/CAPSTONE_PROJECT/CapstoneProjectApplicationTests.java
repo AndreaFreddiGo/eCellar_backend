2d7dbbf30586fd4fee1851af36514027ab3694fb
Andrea Freddi <56250515+AndreaFreddiGo@users.noreply.github.com>
2025-04-16 11:08:53 +0200

    Initial commit

---
+++ src/test/java/andrea_freddi/CAPSTONE_PROJECT/CapstoneProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package andrea_freddi.CAPSTONE_PROJECT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
