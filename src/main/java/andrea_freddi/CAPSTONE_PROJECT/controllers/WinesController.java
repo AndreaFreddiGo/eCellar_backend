9858ebc1359cb6cd955b00a94759c1bf62eb9d69
Andrea Freddi <56250515+AndreaFreddiGo@users.noreply.github.com>
2025-04-23 01:41:09 +0200

    Updates WinesController

--- src/main/java/andrea_freddi/CAPSTONE_PROJECT/controllers/WinesController.java
+++ src/main/java/andrea_freddi/CAPSTONE_PROJECT/controllers/WinesController.java
@@ -1,7 +1,14 @@
 package andrea_freddi.CAPSTONE_PROJECT.controllers;
 
+import andrea_freddi.CAPSTONE_PROJECT.elasticsearch.WineDocument;
+import andrea_freddi.CAPSTONE_PROJECT.services.WinesSearchService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /*
  * This class is a Spring Boot REST controller that handles requests related to wines.
@@ -10,5 +17,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/wines")
+@RequiredArgsConstructor
 public class WinesController {
+
+    private final WinesSearchService winesSearchService;
+
+    @GetMapping("/search")
+    public List<WineDocument> searchWines(@RequestParam String query) {
+        return winesSearchService.search(query);
+    }
 }
