2d5641593ace5e49914f8b29e60a9b19c49b6044
Andrea Freddi <56250515+AndreaFreddiGo@users.noreply.github.com>
2025-04-29 00:29:57 +0200

    Creates mappers

---
+++ src/main/java/andrea_freddi/CAPSTONE_PROJECT/mappers/CellarMapper.java
@@ -0,0 +1,34 @@
+package andrea_freddi.CAPSTONE_PROJECT.mappers;
+
+import andrea_freddi.CAPSTONE_PROJECT.entities.Cellar;
+import andrea_freddi.CAPSTONE_PROJECT.payloads.CellarDTO;
+import andrea_freddi.CAPSTONE_PROJECT.payloads.CellarWineDTO;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+// This class is responsible for mapping between Cellar entities and CellarDTOs
+
+@Component
+public class CellarMapper {
+
+    // Converts a Cellar entity to a CellarDTO
+    public CellarDTO cellarToDTO(Cellar cellar) {
+        if (cellar == null) return null;
+
+        List<CellarWineDTO> cellarWinesDTO = cellar.getCellarWines() != null
+                ? cellar.getCellarWines().stream()
+                .map(CellarWineMapper::cellarWineToDTO)
+                .toList()
+                : null;
+
+        return new CellarDTO(
+                cellar.getId(),
+                cellar.getName(),
+                cellar.getDescription(),
+                cellar.getUser() != null ? cellar.getUser().getId() : null,
+                cellarWinesDTO
+        );
+    }
+}
+
