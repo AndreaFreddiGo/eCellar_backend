64cd17bac969dbe6c0cbe5d080cfa853df979ae0
Andrea Freddi <56250515+AndreaFreddiGo@users.noreply.github.com>
2025-05-02 20:07:29 +0200

    Updates LoginDTO

--- src/main/java/andrea_freddi/eCellar_backend/payloads/LoginDTO.java
+++ src/main/java/andrea_freddi/eCellar_backend/payloads/LoginDTO.java
@@ -3,7 +3,12 @@ package andrea_freddi.eCellar_backend.payloads;
 // This class is used to return the token to the client after login
 // I use Payload for what I need to receive in construction and DTO for what I return
 
+import java.util.UUID;
+
 public record LoginDTO(
-        String token
+        String token,
+        String username,
+        UUID userId,
+        String profilePicture
 ) {
 }
